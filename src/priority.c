165e08247241ff8aff206b2fd948397c1b47e76a
778-lab <jesusjociel368@gmail.com>
2025-12-04 16:41:35 -0300

    atualização do algoritmo de prioridade

--- src/priority.c
+++ src/priority.c
@@ -28,7 +28,7 @@ int main() {
         scanf("%d", &p[i].prioridade);
     }
 
-    // Ordenação por prioridade (menor prioridade primeiro)
+    // Ordenação por prioridade
     for(int i = 0; i < n; i++) {
         for(int j = i + 1; j < n; j++) {
             if(p[j].prioridade < p[i].prioridade) {
@@ -39,38 +39,4 @@ int main() {
         }
     }
 
-    // Cálculo dos tempos
-    p[0].tempo_espera = 0;
-    p[0].turnaround = p[0].burst;
 
-    for(int i = 1; i < n; i++) {
-        p[i].tempo_espera = p[i-1].tempo_espera + p[i-1].burst;
-        p[i].turnaround = p[i].tempo_espera + p[i].burst;
-    }
-
-    // Exibição dos resultados
-    printf("\n\n----- RESULTADOS (Priority Scheduling) -----\n");
-    printf("ID\tBurst\tPrioridade\tEspera\tTurnaround\n");
-
-    for(int i = 0; i < n; i++) {
-        printf("%d\t%d\t%d\t\t%d\t%d\n",
-            p[i].id, p[i].burst, p[i].prioridade,
-            p[i].tempo_espera, p[i].turnaround
-        );
-    }
-
-    // Médias
-    float mediaEspera = 0, mediaTurn = 0;
-    for(int i = 0; i < n; i++) {
-        mediaEspera += p[i].tempo_espera;
-        mediaTurn += p[i].turnaround;
-    }
-
-    mediaEspera /= n;
-    mediaTurn /= n;
-
-    printf("\nTempo medio de espera: %.2f", mediaEspera);
-    printf("\nTempo medio de retorno: %.2f\n", mediaTurn);
-
-    return 0;
-}
